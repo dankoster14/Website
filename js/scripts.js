d5fb341c1a140e3dead244711fc60b79cb0ae472
Daniel Koster <dankoster14@gmail.com>
2017-12-07 10:14:14 +0000

    added extra pages

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//mobile nav menu
+(function(){
+  let menuButton = document.getElementById("menu");
+  let navMenu = document.getElementById("nav-menu");
+
+  menuButton.addEventListener("click, toggleMenu");
+
+  let toggle = false;
+  function toggleMenu(){
+    if(toggle){
+      navMenu.classList.remove("show-menu"), toggle = false
+    }
+    else {
+      navMenu.classList.add("show-menu"), toggle = true
+    }
+  }
+})();
