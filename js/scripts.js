eb7ff8767f58057e248a205f7fd413f80c1fd9b4
Daniel Koster <dankoster14@gmail.com>
2017-12-14 09:56:21 +0000

    added google maps api

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+"use strict";
 //mobile nav menu
 (function(){
   let menuButton = document.getElementById("menu");
@@ -17,3 +18,28 @@
     }
   }
 })();
+
+//google maps api
+function initMap(){
+  let leicester = {lat: 52.6333, lng: -1.1333};
+  let dmu = {lat: 52.629311, lng: -1.137836};
+  let rileys =  {lat: 52.629307, lng: -1.136229};
+  let mapDmu = document.getElementById("map");
+
+  let map = new google.maps.Map(mapDmu, {
+    zoom: 15,
+    center: leicester
+  });
+
+  let markerDMU = new google.maps.Marker({
+    position: dmu,
+    map: map,
+    title: 'DMU campus location'
+  });
+
+  let markerRILEYS = new google.maps.Marker({
+    position: rileys,
+    map: map,
+    title: 'Rileys location'
+  });
+}
